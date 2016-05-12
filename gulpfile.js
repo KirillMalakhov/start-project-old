f97a3446398c959366be3f2d6ac386c12c271d5b
Kirill74 <vtnhjy2832@gmail.com>
2016-05-12 15:11:10 +0300

    work version

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,8 @@ const uglify = require('gulp-uglify'); //Минифицирует js код
 const concat = require('gulp-concat'); //Конкатинирует файлы
 const stylus = require('gulp-stylus'); //Подключает препроцессор stylus
 const autoprefixer = require('autoprefixer-stylus'); //Добавляет автоматическое добавление префиксов
-const browserSync = require('browser-sync').create(); // Запускает веб сервер с livereload
+const browserSync = require('browser-sync'); // Запускает веб сервер с livereload
+const clean = require('gulp-clean');
 
                                     /* РАБОТА С КАРТИНКАМИ */
 const imagemin = require('gulp-imagemin');
@@ -56,7 +57,7 @@ gulp.task('stylus', function(){
                                           /* JADE */
  gulp.task('jade', function(){
      console.log('-----------Собирается JADE');
-     gulp.src('./src/blocks/**/*.jade')
+      gulp.src('./src/blocks/**/*.jade')
         .pipe(plumber(function(error){
             gutil.log(gutil.colors.red(error.message)); //Красит ошибку в красный цвет
             this.emit('end');
@@ -67,12 +68,103 @@ gulp.task('stylus', function(){
         .pipe(gulp.dest('build/'))
         .pipe(notify("JADE COMPILED TO HTML"))
  });
-                                         /* Отчистка build */
+                                  /* Копирование и оптимизация изображений */ //Нужно править
+gulp.task('img', function () {
+  console.log('---------- Копирование и оптимизация картинок');
+ 
+    return gulp.src('src/images/*.*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [
+                {removeViewBox: false},
+                {cleanupIDs: false}
+            ],
+            // use: [pngquant()]
+        }))
+        .pipe(gulp.dest('build/images'));
+        
+        
+});    
+
+                                         /* Отчистка build */ 
+
 gulp.task('clean', function () {
-  console.log('---------- Очистка папки сборки');
-  return del([
-    dirs.build + '/**/*',
-    '!' + dirs.build + '/readme.md'
-  ]);
+    console.log('---------- Очистка папки сборки');
+	return gulp.src('build/*', {read: false})
+		.pipe(clean());
+});
+
+
+
+                                        /* DEBUG */
+gulp.task('debug', function () {
+	return gulp.src('build/**/*')
+		.pipe(debug({title: 'debug:'}))
+		.pipe(gulp.dest('build'));
+});
+
+
+                            /*Конкатенация и углификация Javascript*/
+gulp.task('js', function (callback) {
+  
+    console.log('---------- Обработка JS');
+    return gulp.src('src/js/**/*.js')
+      .pipe(debug({title: "JS:"}))
+      .pipe(gulpIf(isDev, sourcemaps.init()))
+      .pipe(concat('script.min.js'))
+      .pipe(uglify())
+      .on('error', notify.onError(function(err){
+        return {
+          title: 'Javascript uglify error',
+          message: err.message
+        }
+      }))
+      .pipe(gulpIf(isDev, sourcemaps.write('.')))
+      .pipe(gulpIf(isDev, debug({title: "JS SOURCEMAPS:"})))
+      .pipe(gulp.dest(dirs.build + '/js'));
+  
+ 
+});
+
+
+                                           
+
+
+                                            /* ЗАПУСК СЕРВЕРА */
+
+
+
+
+gulp.task('browser-sync', () => {
+    browserSync(
+         {
+            server: 'build',
+            baseDir: 'build',
+            directory: true,
+            tunnel: false,
+            host: 'localhost',
+            port: 9000,
+            injectChanges: true,
+            logPrefix: "App"
+        }
+    );
+});
+
+                                /*Сборка всего*/
+gulp.task('build', ['clean','jade','stylus','img','js', 'debug'], function(){
+    return gulp.src('build')
+    .pipe(notify({
+        title: 'COMAND BUILD',
+        message: 'Completed',
+        
+    }));
+});
+                                    /* Слежение */
+
+gulp.task('start',['build' ,'browser-sync'], (cb) => {
+    gulp.watch('src/**/*.styl', ['stylus']);
+    gulp.watch('src/**/*.jade', ['jade']);
+    gulp.watch('src/**/*.js', ['js']);
+    gulp.watch('src/images/**/*.*', ['img`']);
     
 });
